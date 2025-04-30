916cb9958cc062291d73900712db6cb4a0e1c427
HairuMossa <117511087+HairuMossa@users.noreply.github.com>
2025-04-30 09:16:16 +0300

    Update ooreneo.ino

--- example/oOreNEO/ooreneo/ooreneo.ino
+++ example/oOreNEO/ooreneo/ooreneo.ino
@@ -1,20 +1,14 @@
-#include "ooreneo.h" // include the header file for the class
+#include "ooreneo.h"
 
-
-
-void setup()
-{
-Serial.begin(115200);
-
+void setup() {
+ Serial.begin(115200);
 }
 
 void loop()
 {
-  ooreneo.begin();
-int a = ooreneo.analogRead(0);
+  int a = ooreneo.analogRead(0);
   Serial.print(a);
   Serial.println(" Analog Read \n");
   delay(500);
-
 
 }
